5990e42294d333c4151adc1d6ae6094bd2b1b5d0
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2024-06-02 16:05:39 -0300

    Navigation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,16 +3,28 @@ import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group });
+  }
   return (
     <Container>
       <Header showBackButton />
       <Content>
         <Icon />
-        <Highlight title="Nova turma" subtitle="crie a turma para adicionar pessoas"/>
-        <Input placeholder="Nome da turma"/>
-        <Button title="Criar" style={{marginTop: 20}}/>
+        <Highlight
+          title="Nova turma"
+          subtitle="crie a turma para adicionar pessoas"
+        />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,15 +9,23 @@ import { useState } from "react";
 import { PlayerCart } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useRoute } from "@react-navigation/native";
+
+type RouteParams = {
+  group: string;
+}
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([]);
+
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
   return (
     <Container>
       <Header showBackButton />
       <Highlight
-        title="Nome da turma"
+        title={group}
         subtitle="adicione a galera e separe os times"
       />
       <Form>
@@ -56,7 +64,7 @@ export function Players() {
           <ListEmpty message="Não há pessoas nesse time" />
         )}
       />
-      <Button title="Remover Turma" type="SECONDATY"/>
+      <Button title="Remover Turma" type="SECONDATY" />
     </Container>
   );
 }
